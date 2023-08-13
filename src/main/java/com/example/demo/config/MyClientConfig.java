7af8ee01dc19bf108368c822815f30859749f7db
GO <vn56@bk.ru>
2023-08-13 14:34:56 +0200

    1 commit

---
+++ src/main/java/com/example/demo/config/MyClientConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import lombok.SneakyThrows;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+
+@Configuration
+public class MyClientConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .usingSsl()
+                .withBasicAuth("elastic", "BtqIY38ii44H_Jo9FTZZ")
+                .build();
+    }
+}
---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import com.example.demo.document.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonService(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(final Person person) {
+        repository.save(person);
+    }
+
+    public Person findById(final String id) {
+        return repository.findById(id).orElse(null);
+    }
+}
