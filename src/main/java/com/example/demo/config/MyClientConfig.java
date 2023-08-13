26a0f4574ec54f8d310a5e6435960fe04c48a536
GO <vn56@bk.ru>
2023-08-13 15:50:15 +0200

    1 commit

--- src/main/java/com/example/demo/config/MyClientConfig.java
+++ src/main/java/com/example/demo/config/MyClientConfig.java
@@ -1,21 +1,8 @@
 package com.example.demo.config;
 
-import lombok.SneakyThrows;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.elasticsearch.client.RestClientBuilder;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
 
 @Configuration
 public class MyClientConfig extends ElasticsearchConfiguration {
